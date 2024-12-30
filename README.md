0866d36c1a4c822a073f42254fa8c418d290d21d
Jessica Smith <5874122+jrods@users.noreply.github.com>
2024-12-30 10:46:25 -0800

    Initial commit
    Includes basic setup of the repo with readme, license, spec, and src.
    
    Starting code will run and display a basic view populated with the `~`
    char in the first row column of the viewer's height.
    
    User can move the cursor with `vi` navigation keys 'h' 'j' 'k' 'l' in
    the `normal` mode.
    
    User can enter `insert` mode pressing the 'i' key and type in characters
    into the buffer. To go back into `normal` mode, press the `<Esc>` key.
    
    Note that any time the 'q' key is pressed in any mode, will quit
    `Rock.cr`.

---
+++ README.md
@@ -0,0 +1,8 @@
+Rock.cr
+===
+
+# Welcome
+Rock is a vim-like text editor written with [Crystal](https://crystal-lang.org).
+
+This is a project to understanding how text editors and Terminal UIs (TUI) work, nothing better than learning to reinvent the wheel!
+
---
+++ src/rock.cr
@@ -0,0 +1,198 @@
+require "termios"
+
+lib LibC
+  struct Winsize
+    ws_row : UInt16
+    ws_col : UInt16
+    ws_xpixel : UInt16
+    ws_ypixel : UInt16
+  end
+
+  # TIOCGWINSZ is a magic number passed to ioctl that requests the current
+  # terminal window size. It is platform dependent (see
+  # https://stackoverflow.com/a/4286840).
+  {% begin %}
+    {% if flag?(:darwin) || flag?(:bsd) %}
+      TIOCGWINSZ = 0x40087468
+    {% elsif flag?(:unix) %}
+      TIOCGWINSZ = 0x5413
+    {% end %}
+  {% end %}
+
+  fun ioctl(fd : Int32, request : UInt64, ...) : Int32
+end
+
+def get_term_dim() : LibC::Winsize
+  ws = LibC::Winsize.new
+  ret = LibC.ioctl(STDOUT.fd, LibC::TIOCGWINSZ, pointerof(ws))
+  if ret < 0
+    raise "Failed to get terminal dimentions"
+  end
+  ws
+end
+
+if !STDIN.tty? || !STDOUT.tty?
+  puts "Error: Not running via terminal emulator."
+  exit 1
+end
+
+# TODO: i forgot why this is needed?
+if LibC.tcgetattr(STDIN.fd, out mode) != 0
+  raise IO::Error.from_errno("tcgetattr")
+end
+
+before = mode
+mode.c_lflag &= ~Termios::LocalMode.flags(ECHO, ICANON).value
+LibC.tcsetattr STDIN.fd, Termios::LineControl::TCSANOW, pointerof(mode)
+
+winsize = get_term_dim
+
+Signal::WINCH.trap do
+  winsize = get_term_dim
+end
+
+STDOUT.write "\e[2J".to_slice
+STDOUT.write "\e[H".to_slice
+
+(0...winsize.ws_row).each do |line|
+  STDOUT.write "~\n".to_slice
+end
+
+quit = false
+
+struct Buffer
+end
+
+struct Cursor
+  property x : UInt32, y : UInt32
+
+  def initialize(@x, @y, @winsize : LibC::Winsize); end
+
+  def next_line
+    @x = 1
+    @y += 1
+  end
+
+  def prev_line
+    # TODO figure out how to position at the end of the previous line
+    # might depend on how i will manage the text
+    @y -= 1 if @y > 0
+  end
+
+  def fwd_x
+    @x += 1 if @x < @winsize.ws_col
+  end
+
+  def back_x
+    @x -= 1 if @x > 1
+  end
+
+  def down_y
+    @y += 1 if @y < @winsize.ws_row
+  end
+
+  def up_y
+    @y -= 1 if @y > 1
+  end
+
+  def move
+    STDOUT.write "\e[#{@y};#{@x}H".to_slice
+  end
+
+  def to_s(io : IO) : Nil
+  end
+end
+
+# TODO: multiple cursors
+# there's a need for at least 2, 1 for normal/insert, 1 for command
+c = Cursor.new 1, 1, winsize
+
+enum Mode
+  Command
+  Normal
+  Insert
+  Visual
+  VisualBlock
+  VisualLine
+end
+
+m = Mode::Normal
+
+STDOUT.write "\e[#{winsize.ws_row - 8};#{0}H".to_slice
+STDOUT.write "\e[2K".to_slice
+STDOUT.write "Mode: #{m}".to_slice
+STDOUT.write "\e[4C".to_slice
+STDOUT.write "Cursor: #{c}".to_slice
+
+while !quit && STDIN.not_nil!
+  STDIN.flush
+  c.move
+  user_input = STDIN.read_char
+  quit = user_input.to_s == "q"
+  case m
+  when Mode::Normal
+    # TODO: move key references to a file
+    c.fwd_x if user_input == 'l'
+    c.down_y if user_input == 'j'
+    c.back_x if user_input == 'h'
+    c.up_y if user_input == 'k'
+    m = Mode::Insert if user_input == 'i'
+    if user_input == ':'
+      m = Mode::Command
+      c.x = 2
+      c.y = winsize.ws_row - 3
+    end
+  when Mode::Command
+    if user_input == '\n'
+      m = Mode::Normal
+    elsif user_input.not_nil!.ord == 127
+      c.back_x
+      c.move
+      user_input = ' '
+      if c.x == 1
+        c.prev_line
+        c.move
+      end
+    elsif user_input == '\e'
+    else
+      c.fwd_x
+    end
+    m = Mode::Normal if user_input == '\e'
+    STDOUT.write user_input.to_s.to_slice unless user_input == '\e'
+    STDOUT.write "\e[#{winsize.ws_row - 3};#{0}H".to_slice
+    STDOUT.write ":".to_slice
+  when Mode::Insert
+    if user_input == '\n'
+      c.next_line
+    elsif user_input.not_nil!.ord == 127
+      c.back_x
+      c.move
+      user_input = ' '
+      if c.x == 0
+        c.prev_line
+        c.move
+      end
+    elsif user_input == '\e'
+    else
+      c.fwd_x
+    end
+    m = Mode::Normal if user_input == '\e'
+    STDOUT.write user_input.to_s.to_slice unless user_input == '\e'
+  end
+
+  # Extra info
+  STDOUT.write "\e[#{winsize.ws_row - 8};#{0}H".to_slice
+  STDOUT.write "\e[2K".to_slice
+  STDOUT.write "Mode: #{m}".to_slice
+  STDOUT.write "\e[4C".to_slice
+  STDOUT.write "Input: #{user_input.not_nil!.bytes}".to_slice
+  STDOUT.write "\e[4C".to_slice
+  STDOUT.write "Cursor: #{c}".to_slice
+end
+
+STDOUT.write "\e[0J".to_slice
+STDOUT.write "\n".to_slice
+LibC.tcsetattr STDIN.fd, Termios::LineControl::TCSANOW, pointerof(before)
+puts "so long gay bowsie!"
+
+0
